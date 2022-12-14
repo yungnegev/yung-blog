b34d2149a7331118b9307a0bbd135fb1ed78b7a4
Yung <neiklotrr@gmail.com>
2022-12-14 14:49:45 +0300

    Routing done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Post from './pages/Post'
+import AddPost from './pages/AddPost'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/post/:id',
+        element: <Post />
+      },
+      {
+        path: '/add-post',
+        element: <AddPost />
+      },
+      {
+        path: '/add-post/:id',
+        element: <AddPost />
+      }
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  }
+])
+
+
 
 const App = () => {
   return (
     <div className='app'>
-      <div className='wrapper'>
-      <h1>The app to come...</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi nemo atque quisquam reiciendis est aliquam ullam aspernatur itaque quasi eius veritatis, eveniet facere vitae, corporis, tempora quod alias aliquid sapiente veniam qui numquam tempore eaque! Numquam tempora voluptatibus modi et.</p>
-      </div>
+      <RouterProvider router={router}/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
 )
