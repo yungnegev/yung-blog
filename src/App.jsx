e587d7b035ea42b3d0d1b537e49dffa83266e4f2
Yung <neiklotrr@gmail.com>
2022-12-14 13:56:53 +0300

    Boilerplate.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <div className='wrapper'>
+      <h1>The app to come...</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi nemo atque quisquam reiciendis est aliquam ullam aspernatur itaque quasi eius veritatis, eveniet facere vitae, corporis, tempora quod alias aliquid sapiente veniam qui numquam tempore eaque! Numquam tempora voluptatibus modi et.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
